690c3a0cfefebf773672caa69ef678b3b3591109
Matthew Macon <92400198+matthewmacon90@users.noreply.github.com>
2023-07-05 09:04:48 -0400

    updated ignore

--- mondayQi/review/s3BrowserScript.js
+++
@@ -1,7 +0,0 @@
-// const jsForce = require('jsforce');
-// console.log(jsForce);
-// const connection = new jsForce.Connection({
-//     loginUrl: 'https://'
-// });
-// console.log('Connection: ', connection);
-
